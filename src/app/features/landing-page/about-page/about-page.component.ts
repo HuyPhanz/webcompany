25d43895f50059228a85658a1cde249c92800d83
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2025-04-28 18:03:06 +0700

    features: render data About Page

--- src/app/features/landing-page/about-page/about-page.component.ts
+++ src/app/features/landing-page/about-page/about-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CompanyDetailService } from '../../company/company-detail/company-detail.service';
+import { CompanyDetailResDTO } from '../../company/company-detail/interface';
+import { CompanyInfoService } from '../../company/company_info/company-info.service';
+import { CompanyInfoResDTO } from '../../company/company_info/interface';
 
 @Component({
   selector: 'app-about-page',
@@ -6,4 +10,27 @@ import { Component } from '@angular/core';
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss'
 })
-export class AboutPageComponent {}
+export class AboutPageComponent implements OnInit {
+// get Api About
+  companyDetailService = inject(CompanyDetailService);
+  dataCompanyDetail: CompanyDetailResDTO [] = []
+
+  getAllDataCompanyDetail(){
+    this.companyDetailService.getAlLDataCompanyDetail().subscribe(data=>{
+      this.dataCompanyDetail = data;
+    })
+  }
+  ngOnInit() {
+    this.getAllDataCompanyDetail();
+    this.getAllDataCompanyInfo();
+  }
+  // get Api Comapny Info
+  companyInfoService = inject(CompanyInfoService);
+  dataCompanyInfo: CompanyInfoResDTO [] = []
+
+  getAllDataCompanyInfo(){
+    this.companyInfoService.getAlLDataCompanyInfo().subscribe(data=>{
+      this.dataCompanyInfo = data;
+    })
+  }
+}
