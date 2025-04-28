c67c5f5ed865d47c12e37247ffb9f4665e567870
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2025-04-28 18:08:48 +0700

    features: crud admin New and get Data NewPage

--- src/app/features/landing-page/news-page/news-page.component.ts
+++ src/app/features/landing-page/news-page/news-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { NewsService } from '../../news/news.service';
+import { NewsResDTO } from '../../news/interface';
 
 @Component({
   selector: 'app-news-page',
@@ -6,6 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './news-page.component.html',
   styleUrl: './news-page.component.scss'
 })
-export class NewsPageComponent {
+export class NewsPageComponent implements OnInit {
+    newService = inject(NewsService);
+    dataNews: NewsResDTO [] = []
 
+    getAllDataNews (){
+      this.newService.getAllNews().subscribe(data => {
+        this.dataNews = data;
+      })
+    }
+    ngOnInit() {
+      this.getAllDataNews();
+    }
 }
+
--- src/app/features/news/news.service.ts
+++ src/app/features/news/news.service.ts
@@ -1,32 +1,33 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { NewsReqDTO, NewsResDTO } from './interface';
+import { NewsReqDTO, NewsResDTO,News } from './interface';
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
   http = inject(HttpClient);
-  apiUrl = 'http://10.0.40.28:8080/api/news';
+  apiUrl = 'http://26.179.251.121:8080/api/news';
 
   // Api-Post
-  createNew(data: NewsReqDTO): Observable<NewsReqDTO> {
-    return this.http.post<NewsReqDTO>(this.apiUrl, data);
+  createNew(data: NewsReqDTO): Observable<NewsResDTO> {
+    return this.http.post<NewsResDTO>(this.apiUrl, data);
   }
   // Api-Get ( Read )
-  // getNewById(id:number): Observable<typeOfNews> {
-  //   return this.http.get<typeOfNews>(`${this.apiUrl}/${id}`)
+  // getNewsById(id: number): Observable<NewsResDTO> {
+  //   return this.http.get<NewsResDTO>(`${this.apiUrl}/${id}`);
   // }
   // Api Get All data
   getAllNews(): Observable<NewsResDTO[]> {
     return this.http.get<NewsResDTO[]>(this.apiUrl);
   }
   //delete
-  deleteNew(id: number): Observable<NewsReqDTO> {
-    return this.http.delete<NewsReqDTO>(`${this.apiUrl}/${id}`);
-  }
-  //update
-  updateNew(id: NewsResDTO): Observable<NewsResDTO> {
+  deleteNew(id: number): Observable<NewsResDTO> {
     return this.http.delete<NewsResDTO>(`${this.apiUrl}/${id}`);
   }
+  //update
+  updateNew(data: News): Observable<NewsResDTO> {
+    return this.http.put<NewsResDTO>(`${this.apiUrl}/${data.id}`, data);
+  }
+
 }
