d38c7b23884948dfac8eeb27e3afad424ed0b703
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2025-05-14 16:46:29 +0700

    features: update contact-page

--- src/app/features/landing-page/contact-page/contact-page.component.ts
+++ src/app/features/landing-page/contact-page/contact-page.component.ts
@@ -1,9 +1,7 @@
-import { Component, inject, OnDestroy } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {
-  AbstractControl,
   NonNullableFormBuilder,
   ReactiveFormsModule,
-  ValidationErrors,
   Validators
 } from '@angular/forms';
 
@@ -12,8 +10,8 @@ import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzSelectModule } from 'ng-zorro-antd/select';
-import { Observable, Observer, Subject } from 'rxjs';
 import { CustomerContactService } from '../../company/customer_contact/customer-contact.service';
+
 @Component({
   selector: 'app-contact-page',
   imports: [
@@ -28,45 +26,27 @@ import { CustomerContactService } from '../../company/customer_contact/customer-
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
-export class ContactPageComponent implements OnDestroy {
+export class ContactPageComponent {
   //service
-  customerContact = inject(CustomerContactService);
-  private fb = inject(NonNullableFormBuilder);
-  private destroy$ = new Subject<void>();
+  contactCustomerService = inject(CustomerContactService);
+  private fb= inject(NonNullableFormBuilder)
   validateForm = this.fb.group({
-    name: this.fb.control('', [Validators.required], [this.userNameAsyncValidator]),
+    name: this.fb.control('', [Validators.required]),
     email: this.fb.control('', [Validators.email, Validators.required]),
     phone: this.fb.control('', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
     message: this.fb.control('', [Validators.required, Validators.minLength(6), Validators.maxLength(1000)]),
     status: ['', [Validators.required]],
-  });
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-  userNameAsyncValidator(control: AbstractControl): Observable<ValidationErrors | null> {
-    return new Observable((observer: Observer<ValidationErrors | null>) => {
-      setTimeout(() => {
-        if (control.value === 'JasonWood') {
-          // you have to return `{error: true}` to mark it as an error event
-          observer.next({ error: true, duplicated: true });
-        } else {
-          observer.next(null);
-        }
-        observer.complete();
-      }, 500);
-    });
-  }
+  })
 
-  submitForm(): void {
+  submitForm(){
     if (this.validateForm.valid) {
       const valueForm = this.validateForm.getRawValue();
-      this.customerContact.createCustomerContact(valueForm).subscribe({
+      this.contactCustomerService.createCustomerContact(valueForm).subscribe({
         next: (res) => {
-          console.log(res);
+          console.log(res);  this.validateForm.reset();
         }
       })
-    } else {
+    }else {
       Object.values(this.validateForm.controls).forEach((control) => {
         if (control.invalid) {
           control.markAsDirty();
