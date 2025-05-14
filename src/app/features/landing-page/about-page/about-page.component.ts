402602c1552668a0eb4244becda04a1a5dcc206d
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2025-05-14 23:14:48 +0700

    features: update about-page.component.html/ts

--- src/app/features/landing-page/about-page/about-page.component.ts
+++ src/app/features/landing-page/about-page/about-page.component.ts
@@ -1,28 +1,41 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { NgStyle } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { ExperienceResDTO } from '../../company-experiences/interface';
 import { CompanyExperiencesService } from '../../company-experiences/company-experiences.service';
+import { AboutIntroService } from '../../about-us-intro/about-us-intro.service';
+import { AboutIntroResDTO } from '../../about-us-intro/interface';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-about-page',
-  imports: [NgStyle],
+  imports: [NgIf, RouterLink],
   templateUrl: './about-page.component.html',
   standalone: true,
   styleUrl: './about-page.component.scss'
 })
 export class AboutPageComponent implements OnInit {
-  //service
+  //service companyExperienceService
   companyExperienceService = inject(CompanyExperiencesService);
+  //service aboutIntroService
+  aboutIntroService = inject(AboutIntroService);
   //data
   companyExperienceOfData: ExperienceResDTO[] = [];
-  //Get Api
+  aboutIntroOfData: AboutIntroResDTO[] = [];
+  //Get Api companyExperienceService
   getAllCompanySupport() {
     this.companyExperienceService.getAllData().subscribe((res) => {
       this.companyExperienceOfData = res;
     });
   }
+  //Get Api About Intro
+  getAllAboutIntro() {
+    this.aboutIntroService.getAllData().subscribe((res) => {
+      this.aboutIntroOfData = res;
+    });
+  }
   ngOnInit() {
     this.getAllCompanySupport();
+    this.getAllAboutIntro();
   }
 }
