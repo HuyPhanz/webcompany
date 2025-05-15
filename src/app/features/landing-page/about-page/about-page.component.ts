c767d367dba38e8c6d88ab55a3a6a81f26662983
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2025-05-15 10:39:03 +0700

    features: update about page

--- src/app/features/landing-page/about-page/about-page.component.ts
+++ src/app/features/landing-page/about-page/about-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { NgForOf, NgIf, NgStyle } from '@angular/common';
 import { ExperienceResDTO } from '../../company-experiences/interface';
 import { CompanyExperiencesService } from '../../company-experiences/company-experiences.service';
 import { AboutIntroService } from '../../about-us-intro/about-us-intro.service';
 import { AboutIntroResDTO } from '../../about-us-intro/interface';
 import { RouterLink } from '@angular/router';
+import { CompanyManagerService } from '../../company-manager/company-manager.service';
+import { ManagerResDTO } from '../../company-manager/interface';
 
 
 @Component({
   selector: 'app-about-page',
-  imports: [NgIf, RouterLink],
+  imports: [NgIf, RouterLink, NgStyle, NgForOf],
   templateUrl: './about-page.component.html',
   standalone: true,
   styleUrl: './about-page.component.scss'
@@ -19,9 +21,14 @@ export class AboutPageComponent implements OnInit {
   companyExperienceService = inject(CompanyExperiencesService);
   //service aboutIntroService
   aboutIntroService = inject(AboutIntroService);
+  //service companyManagerService
+  companyManagerService = inject(CompanyManagerService);
   //data
   companyExperienceOfData: ExperienceResDTO[] = [];
   aboutIntroOfData: AboutIntroResDTO[] = [];
+  // data companyManagerService
+  topMembers: ManagerResDTO[] = [];
+  bottomMembers: ManagerResDTO[] = [];
   //Get Api companyExperienceService
   getAllCompanySupport() {
     this.companyExperienceService.getAllData().subscribe((res) => {
@@ -34,8 +41,16 @@ export class AboutPageComponent implements OnInit {
       this.aboutIntroOfData = res;
     });
   }
+  //Get Api About Intro
+  getAllcompanyExperience() {
+    this.companyManagerService.getAllData().subscribe((res) => {
+      this.topMembers = res.slice(0, 2); // 2 phần tử đầu
+      this.bottomMembers = res.slice(-4); // 4 phần tử cuối
+    });
+  }
   ngOnInit() {
     this.getAllCompanySupport();
     this.getAllAboutIntro();
+    this.getAllcompanyExperience();
   }
 }
