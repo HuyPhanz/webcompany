41bdd134c2129c82b27b144587f0252249ba7cc1
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2025-05-19 08:43:07 +0700

    features: update auth

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,18 +1,27 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem('token');
-    if (token) {
-      req = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-    }
-    return next.handle(req);
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+
+  const accessToken = localStorage.getItem('auth_token');
+  if (accessToken) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
   }
-}
+  return next(req).pipe(
+    catchError(error => {
+      if (error instanceof HttpErrorResponse && error.status === 403) {
+        // refresh token
+
+        authService.logout();
+      }
+      return throwError(() => error);
+    })
+  );
+};
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -11,31 +11,46 @@ export class AuthService {
   private tokenKey = 'auth_token';
   private roleKey = 'user_role';
   private apiUrl = environment.API_URL;
+  private userNameKey = 'user_name'; // save user login
+
 
   httpClient = inject(HttpClient);
   router = inject(Router);
 
   private token = signal<string | null>(localStorage.getItem(this.tokenKey));
   role = signal<string | null>(localStorage.getItem(this.roleKey));
-
+  // lưu tên người dùng
+  private userName = signal<string | null>(localStorage.getItem(this.userNameKey));
   isAuthenticated = computed(() => !!this.token());
 
+  // get User
+  getUserName(): string {
+    return this.userName() || 'Admin';
+  }
+  // login
   login(userData: any): void {
     this.httpClient.post<LoginRes>(this.apiUrl + '/auth/login', userData).subscribe({
       next: (res) => {
         console.log(res);
         localStorage.setItem(this.tokenKey, res.token);
         localStorage.setItem(this.roleKey, res.role);
+        localStorage.setItem(this.userNameKey, res.username);
         this.token.set(res.token);
-        this.router.navigate(['/']);
+        this.router.navigate(['/app']);
+
+        this.userName.set(res.username);
       }
     });
   }
-
+  // logout
   logout(): void {
     localStorage.removeItem(this.tokenKey);
     localStorage.removeItem(this.roleKey);
     this.token.set(null);
     this.role.set(null);
+    this.router.navigate(['/auth']);
+
+    localStorage.removeItem(this.userNameKey); // 👈 xoá tên
+    this.userName.set(null); // 👈 clear tên
   }
 }
